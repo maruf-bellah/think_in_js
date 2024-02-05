695cd3bcb74bf4fa5af081f9d69a0afa14c45ae6
maruf bellah <mdmaruf5511507@gmail.com>
2024-02-05 23:53:13 +0600

    note primitive and reference

--- prmitiveVsReference/index.js
+++ prmitiveVsReference/index.js
@@ -1,1 +1,5 @@
-console.log("hello world");
+// In JavaScript, there are two types of data:
+
+// 1= primitive types and reference types. Primitive types are simple data types that hold a single value, such as a number or string.
+
+// 2=  Reference types are complex data types that can hold multiple values, such as an array or object
