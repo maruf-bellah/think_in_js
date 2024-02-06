3dbd328493c9f14b06a7caa25805ff9763ec1e58
maruf bellah <mdmaruf5511507@gmail.com>
2024-02-06 23:21:46 +0600

    some code for primitive and reference js

--- prmitiveVsReference/index.js
+++ prmitiveVsReference/index.js
@@ -3,3 +3,30 @@
 // 1= primitive types and reference types. Primitive types are simple data types that hold a single value, such as a number or string.
 
 // 2=  Reference types are complex data types that can hold multiple values, such as an array or object
+
+// = primitive values
+// 1 = number
+// 2 = String
+// 3 = Boolean
+// 4 = undefined
+// 5 =null
+// 6= Symbol
+const age = 22;
+const name = "maruf belllah";
+const loading = false;
+const user = undefined;
+const response = null;
+const counter = Symbol("counter");
+
+// ReferenceE Value
+
+// 1=anything that si "typeof" "object"
+// 2=objects
+// 3=arrays
+// 4=function
+
+const users = { name: "Maruf" };
+const friends = ["maruf", "bellah"];
+const doThing = () => {
+  console.log("hello i am function");
+};
